58312432fb7d3b0d439f88974f042652f755c485
Rahma Musse <rmusse@umich.edu>
2025-10-14 21:48:01 -0400

    introduction

---
+++ Project1.py
@@ -0,0 +1,20 @@
+# SI 201 Project 1
+# Your name: Rahma Musse
+# Your student id: 31746890
+# Your email: rmusse@umich.edu 
+# Who or what you worked with on this homework (including generative AI like ChatGPT): 
+# Amani(did calculate_body_flipper_to_mass_ratio function and test cases for that function) 
+# Isa(did load_penguin and calculate_average_body_mass_species and test cases for the functions)
+# I did the analyze_bill_ratio_mass_relation function and the test cases for that function)
+# All worked on the write_results function together
+# If you worked with generative AI also add a statement for how you used it. 
+# e.g.: Used chatgpt to help debug and guide me on writing the function
+# Asked Chatgpt hints for debugging and suggesting the general sturcture of the code
+
+
+
+
+# Topic question: Is there a correlation between sex and penguin body mass?
+# Description: Understand the relation of penguin part size (flipper and beak) to rest of the penguin mass and how it related to sex
+
+
